e6b2713b1727aaeabb2820983df440bf10482bc2
Cihad <cihadp@gmail.com>
2023-07-12 01:35:22 +0300

    dasdasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
+import { readFileSync } from 'node:fs';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+  integrations: [
+    {
+      name: "deneme",
+      hooks: {
+        "astro:config:setup": function (options) {
+          const { injectScript } = options
+
+          injectScript(
+            'head-inline',
+            readFileSync(new URL('./integrations/custom.js', import.meta.url), { encoding: 'utf-8' })
+          );
+        }
+      }
+    }
+  ]
+
+});
