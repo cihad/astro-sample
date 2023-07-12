63f1c320723d70850927b4519a9ede3144fca4de
Cihad Paksoy <cihadp@gmail.com>
2023-07-12 19:25:29 +0300

    sad ad ad a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path'
+
 
 // https://astro.build/config
-export default defineConfig({
+const config = defineConfig({
 
   integrations: [
     {
@@ -15,9 +17,30 @@ export default defineConfig({
             'head-inline',
             readFileSync(new URL('./integrations/custom.js', import.meta.url), { encoding: 'utf-8' })
           );
-        }
+        },
+        // 'astro:server:setup': function(options) {
+        //   console.log(options)
+        //   const { server } = options
+        //   const { middlewares } = server
+
+        //   middlewares.use(function(req, res, next) {
+
+        //     if (req.url?.startsWith('/editor')) {
+        //       res.setHeader("x-custom", "cihad")
+        //       res.end(readFileSync(new URL('./editor/index.html', import.meta.url), { encoding: 'utf-8' }))
+
+        //     } else {
+        //       next()
+
+        //     }
+        //   })
+        // }
       }
     }
   ]
 
 });
+
+
+
+export default config
--- integrations/custom.js
+++ integrations/custom.js
@@ -1,1 +1,36 @@
-// alert("merhab")
+alert("hello")
+
+document.addEventListener('mouseover', (e) => {
+  console.log(e.target);
+  if (!e.target) return;
+
+  const target = e.target;
+  const payload = {
+    rect: target.getBoundingClientRect(),
+    element: target.nodeName,
+  };
+
+  if (target instanceof HTMLParagraphElement) {
+    payload['innerText'] = target.innerText;
+  }
+
+  window.parent.postMessage(
+    {
+      type: "hover",
+      payload,
+    },
+    '*'
+  );
+});
+
+window.addEventListener('message', (e) => {});
+
+document.addEventListener('dblclick', (e) => {
+  const target = e.target;
+  if (e.target instanceof HTMLParagraphElement) {
+    const response = prompt('New text', target.innerText);
+
+    if (!response) return;
+    target.innerText = response;
+  }
+});
